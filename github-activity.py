3701d9c864f115159ea9556631c4d450197c5c43
abydow <Okamiwolfie@outlook.com>
2025-10-15 17:31:08 +0530

    Update

--- github-activity.py
+++ github-activity.py
@@ -8,9 +8,15 @@ shows:Github user activity using built in python
 """
 
 import sys
-import json 
+import json
 from urllib.request import urlopen,Request
 from urllib.error import HTTPError,URLError
-from datetime import datetime 
+from datetime import datetime
 
+def print_usage():
+    # Print usage instructions.
+    print("Usage: python github-activity.py <github-username>")
+    print("Example:")
+    print("python github-activity.py abydow")
+ 
 
