a10609e8c6851963247f56bcf40504ca1c4b44c0
abydow <Okamiwolfie@outlook.com>
2025-10-16 15:46:45 +0530

    Update

--- github-activity.py
+++ github-activity.py
@@ -17,6 +17,22 @@ def print_usage():
     # Print usage instructions.
     print("Usage: python github-activity.py <github-username>")
     print("Example:")
-    print("python github-activity.py abydow")
- 
+    print("python github-activity.py abydow") 
 
+def main():
+    #important function(main)
+    #checks command line arg
+    if len(sys.argv) != 2:
+        print_usage()
+        sys.exit(1)
+
+    username = sys.argv[1]
+
+    #Validation check<username>
+    if not username  or not username.replace('-','').replace('_','').isalnum():
+        print("ERROR:")
+        print("Please Enter a valid USername")
+        sys.exit(1)
+
+   print(f"Fetching activity for github user: {usernamer}")
+   print("-" * 50)
